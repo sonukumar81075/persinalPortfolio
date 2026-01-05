29626dbc7ae48029d1cbacedd6b161cb198eb03f
sonu kumar saini <sonusaini81075@gmail.com>
2026-01-05 13:11:25 +0530

    updated code

--- src/components/SplashCursor.jsx
+++ src/components/SplashCursor.jsx
@@ -1198,11 +1198,11 @@ function SplashCursor({
     TRANSPARENT,
   ]);
 
-  return (
-    <div className="fixed top-0 left-0 z-50 pointer-events-none w-full h-full">
-      <canvas ref={canvasRef} id="fluid" className="w-screen h-screen block"></canvas>
-    </div>
-  );
+  // return (
+    // <div className="fixed top-0 left-0 z-50 pointer-events-none w-full h-full">
+    //   <canvas ref={canvasRef} id="fluid" className="w-screen h-screen block"></canvas>
+    // </div>
+  // );
 }
 
 export default SplashCursor;
